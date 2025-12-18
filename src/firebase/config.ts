4703b4012bdfa209be9f361f51cbb110835abfd1
Abbas Challawala <abbaschallawala53@gmail.com>
2025-12-18 14:45:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1967272411-5ca25",
+  "appId": "1:590587431614:web:5f11553133b57508c96cef",
+  "apiKey": "AIzaSyCVBT9e-Y6Hu8O2YytOBepPzsxF_UOGBFg",
+  "authDomain": "studio-1967272411-5ca25.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "590587431614"
+};
